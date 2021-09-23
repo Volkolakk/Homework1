f25ed891cf46336caa03e4fd8a750ae54397f58c
Volkolakk <91282980+Volkolakk@users.noreply.github.com>
2021-09-23 21:32:07 +0300

    Update Homework1.c

--- Homework1.c
+++ Homework1.c
@@ -3,20 +3,20 @@
 
 int main()
 {
-    int number; // Огранчение: Входные числа не более 2^30
+    int number; 
     char number10[sizeof(int) * 8 + 1];
     number10[sizeof(int) * 8] = '\0';
     int i = 0;
 
-    scanf_s("%9d", &number);// Принимаем число от -99999999 до 999999999
+    scanf_s("%9d", &number);// РџСЂРёРЅРёРјР°РµРј С‡РёСЃР»Рѕ РѕС‚ -99999999 РґРѕ 999999999
 
-    while (getchar() != '\n') { // Проверка на корректность введенных данных
+    while (getchar() != '\n') { // РџСЂРѕРІРµСЂРєР° РЅР° РєРѕСЂСЂРµРєС‚РЅРѕСЃС‚СЊ РІРІРµРґРµРЅРЅС‹С… РґР°РЅРЅС‹С…
         printf("try again write correct number\n");
         while (getchar() != '\n');
         scanf_s("%d", &number);
     }
 
-    while (i <= (sizeof(int) * 8 - 1)) { //Цикл записывает число в двоичной системе в массив 
+    while (i <= (sizeof(int) * 8 - 1)) { //Р¦РёРєР» Р·Р°РїРёСЃС‹РІР°РµС‚ С‡РёСЃР»Рѕ РІ РґРІРѕРёС‡РЅРѕР№ СЃРёСЃС‚РµРјРµ РІ РјР°СЃСЃРёРІ
         if ((number & (1 << i)) != 0)
             number10[(sizeof(int) * 8 - 1) - i] = '1';
         else
@@ -24,5 +24,5 @@ int main()
         i++;
     }
 
-    printf("Answer:  %s", number10); //Печатаем ответ
+    printf("Answer:  %s", number10); //РџРµС‡Р°С‚Р°РµРј РѕС‚РІРµС‚
 }
